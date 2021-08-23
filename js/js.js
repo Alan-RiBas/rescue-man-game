060dd56ae5ad811033cc01663e0d3fe7883b6dc5
Alan-RiBas <alanbatista.ribas@gmail.com>
2021-08-23 00:01:07 -0300

    adiciona funçao de movimento W S, subir e descer.

--- js/js.js
+++ js/js.js
@@ -9,23 +9,64 @@ function start(){//Inicio da função start()
 
     //Váriaveis principais
 
-var jogo = {}
+    var jogo = {}
+    var TECLA = {
+        W: 87,
+        S:83,
+        D: 68
+    }
 
-//game Loop
+    jogo.pressionou = [];
 
-jogo.timer = setInterval(loop, 30);
+    //Váriaveis principais
 
-function loop() {
+    //verifica se uma tecla foi pressionada
 
-    moveFundo();
+    $(document).keydown(function(e) {
+        jogo.pressionou[e.which] = true;
+    });
 
-} //Fim da Funçao gameLoop
+    $(document).keyup(function(e) {
+        jogo.pressionou[e.which] = false;
+    });
+
     
-//Função movimenta o fundoGame
-function moveFundo(){
-    esquerda = parseInt($("#fundoGame").css("background-position"));
-    $("#fundoGame").css("background-position", esquerda-3);
-}
+
+
+    //game Loop
+
+    jogo.timer = setInterval(loop, 30);
+
+    function loop() {
+
+        moveFundo();
+        moveJogador();
+
+    } //Fim da Funçao gameLoop
+
+    //Função movimenta o fundoGame
+    function moveFundo(){
+        esquerda = parseInt($("#fundoGame").css ("background-position"));
+        $("#fundoGame").css("background-position",  esquerda-3);
+    };
+
+    //Fim da função movimenta o fundoGame
+
+    function moveJogador(){
+        if(jogo.pressionou[TECLA.W]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo-10);
+        };
+
+        if(jogo.pressionou[TECLA.S]){
+            var topo = parseInt($("#jogador").css("top"));
+            $("#jogador").css("top", topo+10);
+        }
+
+        if(jogo.pressionou[TECLA.D]){
+            //chama função disparo
+        }
+    }//Fim da função moveJogador
 
 } //Fim da função start
 
